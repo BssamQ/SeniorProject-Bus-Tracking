dec7d2755a25cc684ee890c609bd97618b2ce728
BassaM Q <bssam5050@gmail.com>
2025-02-02 04:44:34 +0300

    auth

--- bus_tracking_server/bin/main.dart
+++ bus_tracking_server/bin/main.dart
@@ -1,7 +1,23 @@
 import 'package:bus_tracking_server/server.dart';
+import 'package:serverpod/serverpod.dart';
+
+import 'package:serverpod_auth_server/serverpod_auth_server.dart' as auth;
 
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
 void main(List<String> args) {
   run(args);
+
+
+}
+
+void run(List<String> args) async {
+  var pod = Serverpod(
+    args,
+    auth.Protocol(),
+    auth.Endpoints(),
+    authenticationHandler: auth.authenticationHandler, // Add this line
+  );
+
+  await pod.start();
 }
